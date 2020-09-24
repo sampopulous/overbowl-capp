7e372f3c02af9af272066a31f3be544ae51c9950
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-09-24 15:45:43 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={				
-	'ApiUrl':"https://overbowl.in/dev/mobileappv2/api",	
+	'ApiUrl':"https://overbowl.in/mobileappv2/api",	
 	'AppTitle':"Over Bowl",
 	'ApiKey' : 'dfadjdfjjdsdnjdddnbgapokdaKDagdfhsdqd',	
 	'debug': false
